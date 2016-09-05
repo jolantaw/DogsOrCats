6fb4c95e4c8833121f69e1c9bdcbaed29c258a6c
jolantaw <jolanta.wtulich@wp.pl>
2016-09-05 22:08:11 +0200

    zmiany dla psów

--- app/src/main/java/girlscodefun/com/carordog/MainActivity.java
+++ app/src/main/java/girlscodefun/com/carordog/MainActivity.java
@@ -2,12 +2,39 @@ package girlscodefun.com.carordog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    Integer / int-liczby calkowiete
+    Float / float-liczby z przecinkiem
+    String-napis
+    Boolean /boolean -prawda lub fałsz
+     */
+
+    int dogVotesCounter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void voteForDog(View view){
+        TextView dogVotes =(TextView)findViewById(R.id.dog_votes);
+
+        dogVotesCounter = dogVotesCounter + 1;
+
+        dogVotes.setText( "" + dogVotesCounter );
+
+    }
+    public class MainActivity extends AppCompatActivity {
+
+        int catVotesCounter =0;
+
+
+
     }
 }
